0b46488dc44620a444fd85a1adf3c70e8f6dad9b
Kirill-Korzunov <kirillkorzunov@gmail.com>
2023-05-17 20:43:50 +0300

    add path(delete)

--- django_test/articles/urls.py
+++ django_test/articles/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='articles'),
     path('<int:id>', views.OneArticleView.as_view(), name='detail_article'),
     path('create', views.ArticleCreateView.as_view(), name='create'),
-    # path('<int:id>/update', views.create, name='update'),
+    path('<int:id>/update', views.ArticleUpdateView.as_view(), name='update'),
     # path('<int:id>/delete', views.create, name='delete'),      
 ]
