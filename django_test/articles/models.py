6cc45df920c82d401c56d38facc879aa074d83f7
Kirill-Korzunov <kirillkorzunov@gmail.com>
2023-05-20 21:18:39 +0300

    set modified default=NULL

--- django_test/articles/models.py
+++ django_test/articles/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField('Название', max_length=255)
     text = models.TextField('Содержание')
-    created = models.DateTimeField('Дата создания',auto_now_add=True)
-    modified = models.DateTimeField('Дата изменения')
+    created = models.DateTimeField('Дата создания', auto_now_add=True)
+    modified = models.DateTimeField('Дата изменения', null=True)
 
     def __str__(self):
         return self.title
